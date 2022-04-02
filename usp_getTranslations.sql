bd521ab3c65e953ce70f1109d06dfc8af9b2235d
unknown <vicunite2@gmail.com>
2022-04-02 12:30:15 +0530

    usp

--- usp_getTranslations.sql
+++ usp_getTranslations.sql
@@ -1,6 +1,16 @@
+Drop Procedure usp_getTranslations
+Go
 Create Procedure usp_getTranslations
 @Language varchar(50)
 as
 Begin
+	If(@Language = 'EN')
+	Begin
 	Select Code,English Translation from table_translation with(nolock)
+	End
+	
+	If(@Language = 'AR')
+	Begin
+	Select Code,Arabic Translation from table_translation with(nolock)
+	End
 End
