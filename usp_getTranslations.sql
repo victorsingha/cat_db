95489f951fc184e49caa3200989bd14826b514ae
unknown <vicunite2@gmail.com>
2022-04-02 13:16:29 +0530

    usp

--- usp_getTranslations.sql
+++ usp_getTranslations.sql
@@ -1,15 +1,15 @@
 Drop Procedure usp_getTranslations
 Go
 Create Procedure usp_getTranslations
-@Language varchar(50)
+@language varchar(50)
 as
 Begin
-	If(@Language = 'EN')
+	If(@language = 'EN')
 	Begin
 	Select Code,English Translation from table_translation with(nolock)
 	End
 	
-	If(@Language = 'AR')
+	If(@language = 'AR')
 	Begin
 	Select Code,Arabic Translation from table_translation with(nolock)
 	End
