485ba803470964ca82ef149ef7af14623c950480
Victor Singha <82039602+victorsingha@users.noreply.github.com>
2022-04-02 12:23:13 +0530

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,9 +0,0 @@
-create table table_Users (UserId int identity(1,1),Username varchar(300),
-Password nvarchar(max),Email varchar(300),PhoneNo varchar(100),Transdate DateTime default getdate(),
-CreatedBy varchar(100),UpdatedBy varchar(100),Verified varchar(30) default 'No')
-
-
-Insert Into table_Users (Username,Password,Email,PhoneNo) values ('admin','123','admin@gmail.com','0000000000')
-
-Insert Into table_Users (Username,Password,Email,PhoneNo) values ('victor','123','victor@gmail.com','1111111111')
-
