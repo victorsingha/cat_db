4127148556a186def33d58d4d454d076a98408d4
unknown <vicunite2@gmail.com>
2022-04-03 23:15:02 +0530

    tr

--- table_user.sql
+++ table_user.sql
@@ -1,10 +1,13 @@
-create table table_user (UserId int identity(1,1),Username varchar(300),
+create table table_user (UserId int identity(1,1),Username nvarchar(300),
 Password nvarchar(max),Email varchar(300),PhoneNo varchar(100),Transdate DateTime default getdate(),
 CreatedBy varchar(100),UpdatedBy varchar(100),Verified varchar(30) default 'No')
 
 
-Insert Into table_user (Username,Password,Email,PhoneNo) values ('admin','123','admin@gmail.com','0000000000')
+Insert Into table_user (Username,Password,Email,PhoneNo) values (N'admin',N'123','admin@gmail.com','0000000000')
 
-Insert Into table_user (Username,Password,Email,PhoneNo) values ('victor','123','victor@gmail.com','1111111111')
+Insert Into table_user (Username,Password,Email,PhoneNo) values (N'victor',N'123','victor@gmail.com','1111111111')
 
 
+delete from table_user
+
+drop table table_user
