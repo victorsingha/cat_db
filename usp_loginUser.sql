5822cdf8f3f698546232b53e23190494c5e13912
unknown <vicunite2@gmail.com>
2022-04-04 21:26:56 +0530

    tr

--- usp_loginUser.sql
+++ usp_loginUser.sql
@@ -7,29 +7,20 @@ Create Procedure usp_loginUser
 @phoneno varchar(50) = null
 as
 Begin
-	Declare @Encrypt varbinary(200),@StatusCode varchar(20) = 'S',@StatusMessage nvarchar(max),@Count int
+	Declare @Encrypt varbinary(200),@StatusCode varchar(20) = 'S',@StatusMessage nvarchar(max),@Count int,
+	@Decrypt nvarchar(100)
+
+	Select @Decrypt = Convert(varchar,DECRYPTBYPASSPHRASE('#@#D3DWWEW$T%^%REDW#',Password))
+	from table_user with(nolock) where username = @username 
 
 
-	Select @Count = Count(*) from table_user with(nolock) where Username = @username and Password = @password
+
+	Select @Count = Count(*) from table_user with(nolock) where Username = @username and @Decrypt = @password
 	If(@Count = 0)
 	Begin
 		Set @StatusCode = 'F'
 		Set @StatusMessage = 'Invalid Credentials'
 	End
-
-	--Select @Count = Count(*) from table_user with(nolock) where Email = @email and Password = @password
-	--If(@Count = 0)
-	--Begin
-	--	Set @StatusCode = 'F'
-	--	Set @StatusMessage = 'Invalid Credentials'
-	--End
-
-	--Select @Count = Count(*) from table_user with(nolock) where PhoneNo = @phoneno and Password = @password
-	--If(@Count = 0)
-	--Begin
-	--	Set @StatusCode = 'F'
-	--	Set @StatusMessage = 'Invalid Credentials'
-	--End
 
 
 	If(@Count > 0)
--- usp_registerUser.sql
+++ usp_registerUser.sql
@@ -7,8 +7,7 @@ Create Procedure usp_registerUser
 @phoneno varchar(50)= null
 as
 Begin
-	Declare @Encrypt varbinary(200),@StatusCode varchar(20) = 'S',@StatusMessage nvarchar(max),@Count int = 0,
-	@Hashbyte binary(64)
+	Declare @Encrypt varbinary(200),@StatusCode varchar(20) = 'S',@StatusMessage nvarchar(max),@Count int = 0
 
 	Select @Count = Count(*) from table_user with(nolock) where Username = @username
 	If(@Count > 0)
@@ -28,17 +27,14 @@ Begin
 
 	If(@StatusCode = 'S')
 	Begin
-		--Select @Encrypt = EncryptByPassPhrase('#@#D3DWWEW$T%^%REDW#', @password )  
-		--@@@@@--Select Convert(varchar,DECRYPTBYPASSPHRASE('#@#D3DWWEW$T%^%REDW#',@Encrypt)) as Decrypt
-		--Insert Into table_user(Username,Password,Email,PhoneNo) values
-		--(@username,@Encrypt,@email,@phoneno)
-
-		--Select @Hashbyte = HASHBYTES('SHA2_512', @password)
-		--Insert Into table_user(Username,Password,Email,PhoneNo) values
-		--(@username,@Hashbyte,@email,@phoneno)
-
+		Select @Encrypt = EncryptByPassPhrase('#@#D3DWWEW$T%^%REDW#', @password )  
 		Insert Into table_user(Username,Password,Email,PhoneNo) values
-		(@username,@password,@email,@phoneno)
+		(@username,@Encrypt,@email,@phoneno)
+		--@@@@@--Select Convert(varchar,DECRYPTBYPASSPHRASE('#@#D3DWWEW$T%^%REDW#',@Encrypt)) as Decrypt
+
+
+		--Insert Into table_user(Username,Password,Email,PhoneNo) values
+		--(@username,@password,@email,@phoneno)
 
 		Set @StatusCode = 'S'
 		Set @StatusMessage = 'Registration successfull'
